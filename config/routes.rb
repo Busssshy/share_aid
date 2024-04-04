d69bd72359cdafb70bbf869793bd55071776ce40
ystm08 <150111719+ystm08@users.noreply.github.com>
2024-04-04 08:18:40 +0000

    Add　devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
+
+  # 自治体用
+  devise_for :local_governments, controllers: {
+    registrations: 'local_government/registrations',
+    sessions: 'local_government/sessions'
+  }
+
+  # ボランティア用
+  devise_for :volunteers, controllers: {
+    registrations: 'volunteer/registrations',
+    sessions: 'volunteer/sessions'
+  }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
