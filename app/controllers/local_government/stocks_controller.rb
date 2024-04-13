24a5fd7009bebdf4838177e34ab2b7c0f7690dbd
suzuki414 <152411546+suzuki414@users.noreply.github.com>
2024-04-13 17:34:12 +0000

    在庫一覧　controller,views　作成

--- app/controllers/local_government/stocks_controller.rb
+++ app/controllers/local_government/stocks_controller.rb
@@ -1,5 +1,11 @@
 class LocalGovernment::StocksController < ApplicationController
+  before_action :authenticate_local_government!
+  
   def new
+  end
+  
+  def index
+    @stocks = Stock.page(params[:page]).per(10)
   end
 
   def edit
