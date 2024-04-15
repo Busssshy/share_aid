1068d88e967118b44ea6e0df340555dfc33c2e09
suzuki414 <152411546+suzuki414@users.noreply.github.com>
2024-04-15 13:32:54 +0000

    在庫登録　削除

--- app/controllers/local_government/stocks_controller.rb
+++ app/controllers/local_government/stocks_controller.rb
@@ -1,10 +1,9 @@
 class LocalGovernment::StocksController < ApplicationController
   before_action :authenticate_local_government!
-  
+
   def new
-    @stock = Stock.new
   end
-  
+
   def index
     @stocks = Stock.page(params[:page]).per(10)
   end
