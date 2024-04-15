7596cac125ad142750e3e16eee9a673591a0e064
suzuki414 <152411546+suzuki414@users.noreply.github.com>
2024-04-15 14:10:19 +0000

    在庫登録　追加

--- app/controllers/local_government/stocks_controller.rb
+++ app/controllers/local_government/stocks_controller.rb
@@ -4,7 +4,27 @@ class LocalGovernment::StocksController < ApplicationController
   def new
     @stock = Stock.new
   end
+  
+  def create
+    @stock = Stock.new(stock_params)
+    @stock.local_government_id = current_local_government.id
+    @stock.save
+    redirect_to local_government_stocks_path
+  end  
 
   def edit
   end
+  
+  private
+  
+  
+  
+  
+  
+  
+  
+  # ↓↓↓追加したカラムを追加する
+  def stock_params
+    params.require(:stock).permit(:name, :amount, :deadline, :memo)
+  end  
 end
