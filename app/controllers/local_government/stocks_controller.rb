f6d9a41d67cfcfdfc71664f886635400fbc8bec0
suzuki414 <152411546+suzuki414@users.noreply.github.com>
2024-04-16 12:51:10 +0000

    在庫更新　修正

--- app/controllers/local_government/stocks_controller.rb
+++ app/controllers/local_government/stocks_controller.rb
@@ -7,7 +7,21 @@ class LocalGovernment::StocksController < ApplicationController
   end
   
   def update
+    @stock = Stock.find(params[:id])
     if @stock.update(stock_params)
-      redirect_to
+      redirect_to local_government_stocks_path
+  end
+  
+  private
+  
+  
+  
+  
+  
+  
+  
+  # ↓↓↓追加したカラムを追加する
+  def stock_params
+    params.require(:stock).permit(:name, :amount, :deadline, :memo)
   end
 end
