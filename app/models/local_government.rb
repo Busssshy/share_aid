3729f12422f571d7bc530578d72ac5a0af24a00c
suzuki414 <152411546+suzuki414@users.noreply.github.com>
2024-04-16 15:10:26 +0000

    models　修正

--- app/models/local_government.rb
+++ app/models/local_government.rb
@@ -3,6 +3,6 @@ class LocalGovernment < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         
+
   has_many :notifications, dependent: :destroy
 end
