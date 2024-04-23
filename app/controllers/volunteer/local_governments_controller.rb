725c7431093ab2c3661bded376233c5e4ef437a1
suzuki414 <152411546+suzuki414@users.noreply.github.com>
2024-04-23 07:33:40 +0000

    不足一覧　コントローラー作成

--- app/controllers/volunteer/local_governments_controller.rb
+++ app/controllers/volunteer/local_governments_controller.rb
@@ -1,9 +1,12 @@
 class Volunteer::LocalGovernmentsController < ApplicationController
+  before_action :authenticate_volunteer!
+  
   def index
     @local_governments = LocalGovernment.all
   end
 
   def show
     @local_government = LocalGovernment.find(params[:id])
+    @shortage = (Stock.local_government.population * Stock.one_daily_quantity - Stock.amount < 0).all
   end
 end
