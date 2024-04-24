e934307f15c7f2e746093fe4da7cd3626fb43dde
suzuki414 <152411546+suzuki414@users.noreply.github.com>
2024-04-24 04:43:38 +0000

    関連付け

--- app/models/volunteer.rb
+++ app/models/volunteer.rb
@@ -3,7 +3,10 @@ class Volunteer < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         
+
   has_many :vol_local_gov_relations, dependent: :destroy
   has_many :local_governments, through: :vol_local_gov_relations
+  has_many :reservation_confirmations, dependent: :destroy
+  has_many :stocks, through: :reservation_confirmations
+  has_many :reservations, dependent: :destroy
 end
