fb96182a59e3e123bb9e03267f921907ac650c0c
suzuki414 <152411546+suzuki414@users.noreply.github.com>
2024-05-06 02:01:41 +0000

    トライ

--- db/schema.rb
+++ db/schema.rb
@@ -79,6 +79,15 @@ ActiveRecord::Schema.define(version: 2024_05_01_012923) do
     t.index ["local_government_id"], name: "index_stocks_on_local_government_id"
   end
 
+  create_table "vol_local_gov_relations", force: :cascade do |t|
+    t.integer "local_government_id", null: false
+    t.integer "volunteer_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["local_government_id"], name: "index_vol_local_gov_relations_on_local_government_id"
+    t.index ["volunteer_id"], name: "index_vol_local_gov_relations_on_volunteer_id"
+  end
+
   create_table "volunteers", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -102,4 +111,6 @@ ActiveRecord::Schema.define(version: 2024_05_01_012923) do
   add_foreign_key "reservation_details", "stocks"
   add_foreign_key "reservations", "volunteers"
   add_foreign_key "stocks", "local_governments"
+  add_foreign_key "vol_local_gov_relations", "local_governments"
+  add_foreign_key "vol_local_gov_relations", "volunteers"
 end
